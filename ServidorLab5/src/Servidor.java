0b4b86053d4163cc9a359f780e72d581a278a54a
henryfvargas22 <henryfvargas@MacBook-Pro-de-Henry.local>
2015-09-17 17:50:03 -0500

    Listo TCP

---
+++ ServidorLab5/src/Servidor.java
@@ -0,0 +1,52 @@
+import java.awt.FontFormatException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
+
+public class Servidor 
+{
+	private ServidorTCP tcp;
+	private ServidorUDP udp;
+	
+	public static String HOLA="HOLA";
+	public static String SELECCIONE="SELECCIONE PROTOCOLO";
+	public static String TCP="TCP";
+	public static String UDP="UDP";
+	
+	public static void main(String argv[]) throws Exception
+	{
+		ServerSocket socket = new ServerSocket(8180);
+		int i = 1;
+		System.out.println("El servidor esta listo para aceptar conexiones.");
+		while (true)
+		{
+			Socket s = socket.accept();
+			PrintWriter writer = new PrintWriter(s.getOutputStream(), true);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			String linea = reader.readLine();
+			
+			if (linea.equals(HOLA)) 
+			{
+				writer.println(SELECCIONE);
+			}
+			
+			linea = reader.readLine();
+			
+			if(linea.equals(TCP))
+			{
+				System.out.println("Protocolo TCP");
+				s.close();
+				ServidorTCP.main(socket);
+			}
+			else if(linea.equals(UDP))
+			{
+				System.out.println("Protocolo UDP");
+				s.close();
+				
+			}
+
+			i++;
+		}
+	}
+}
---
+++ ServidorLab5/src/ServidorTCP.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.net.*;
+
+public class ServidorTCP
+{
+
+	public static void main(ServerSocket socket) throws Exception
+	{
+		String clientSentence;
+		String capitalizedSentence;
+		ServerSocket welcomeSocket = socket;
+		while(true) 
+		{
+			Socket connectionSocket = welcomeSocket.accept();
+			BufferedReader inFromClient =
+					new BufferedReader(new
+							InputStreamReader(connectionSocket.getInputStream())); 
+		DataOutputStream outToClient =
+				new DataOutputStream(connectionSocket.getOutputStream());
+				clientSentence = inFromClient.readLine();
+				capitalizedSentence = clientSentence.toUpperCase() + '\n';
+				outToClient.writeBytes(capitalizedSentence);
+		}
+	}
+}
